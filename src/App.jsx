baa8ec872f5910c85ca418de557a3e9ee3cfab8e
Ersin Çöl <ersincol235@gmail.com>
2024-02-03 22:33:51 +0300

    Add Html

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,35 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Sign-Up Form Application</h1>
+      <form>
+          <div className="container">
+              <h1>Sign Up</h1>
+              <p>Please fill in this form to create an account.</p>
+              <hr/>
+
+              <label htmlFor="email"><b>Email</b></label>
+              <input type="email" placeholder="Enter Email" id="email" name="email" required/>
+
+              <label htmlFor="password"><b>Password</b></label>
+              <input type="password" placeholder="Enter Password" id="password" name="password" required/>
+
+              <label htmlFor="password-repeat"><b>Repeat Password</b></label>
+              <input type="password" placeholder="Repeat Password" id="password-repeat" name="password-repeat"
+                     required/>
+
+              <label>
+                  <input type="checkbox" checked name="remember" style={{marginBottom: "1rem"}}/>
+                  Remember me
+              </label>
+
+              <p>By creating an account you agree to our <a href="#" style={{color: "dodgerblue"}}>Terms & Privacy</a>.</p>
+
+              <div className="clearfix">
+                  <button type="button" className="cancel-btn">Cancel</button>
+                  <button type="submit" className="signup-btn">Sign Up</button>
+              </div>
+          </div>
+      </form>
     </>
   )
 }
